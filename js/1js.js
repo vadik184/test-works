6267b04dee1a06f135a6079bd5ae2e378a74c77a
vadik184 <133372327+vadik184@users.noreply.github.com>
2024-04-01 22:29:27 +0300

    мп

--- js/1js.js
+++ js/1js.js
@@ -5,3 +5,45 @@ colorChangeButton.addEventListener("click", () => {
   //   colorOptionList.style.display = "grid";
   colorOptionList.classList.toggle("show-grid");
 });
+
+const addTask = document.querySelector(".add-btn");
+const taskSection = document.querySelector(".task-section");
+
+let cont = 0;
+
+addTask.addEventListener("click", () => {
+  const taskDiv = document.createElement("div");
+  taskDiv.classList.add("card-fild");
+  taskDiv.innerHTML = `
+      <ul class="card-list">
+        <li class="card-item">
+          <div class="task-card">
+            <ul class="inside-card-list show show-grid">
+              <li class="item-title title-color">
+                <h3 class="task-title">Назва</h3>
+                <div class="color-picker">
+                  <button class="color-change"></button>
+                  
+                </div>
+              </li>
+              <li class="inside-text-fild">
+                <p class="task-text">текст</p>
+              </li>
+              <li>
+                <div class="btn-container">
+                  <button type="button" class="edit-btn">
+                    Редагувати
+                  </button>
+                  <button type="button" class="delete-btn">
+                    Видалити
+                  </button>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+    `;
+
+  taskSection.appendChild(taskDiv);
+});
